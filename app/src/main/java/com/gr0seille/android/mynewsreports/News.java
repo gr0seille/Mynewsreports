c4d431623811575f40c26711d0ccefa99c11ca01
gr0seille <unautrecomptetest22@gmail.com>
2019-01-08 21:36:22 +0100

    Initial commit

---
+++ app/src/main/java/com/gr0seille/android/mynewsreports/News.java
@@ -0,0 +1,48 @@
+package com.gr0seille.android.mynewsreports;
+
+
+public class Earthquake {
+    private String webUrl;
+    private String sectionName;
+    private String webTitle;
+    private String webPublicationDate;
+
+    public Earthquake(String webUrl, String sectionName, String webTitle, String webPublicationDate) {
+        this.webUrl = webUrl;
+        this.sectionName = sectionName;
+        this.webTitle = webTitle;
+        this.webPublicationDate = webPublicationDate;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public String getWebTitle() {
+        return webTitle;
+    }
+
+    public void setWebTitle(String webTitle) {
+        this.webTitle = webTitle;
+    }
+
+    public String getWebPublicationDate() {
+        return webPublicationDate;
+    }
+
+    public void setWebPublicationDate(String webPublicationDate) {
+        this.webPublicationDate = webPublicationDate;
+    }
+}
